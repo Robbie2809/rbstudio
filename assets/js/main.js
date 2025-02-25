fd27f5b0036bea0b338dd2b470c87f9578df2869
Robbie2809 <robbie@rbstudio.co.za>
2025-02-25 12:17:15 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,3 +183,16 @@
 			});
 
 })(jQuery);
+
+<script>
+    let index = 0;
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    function showNextSlide() {
+        index = (index + 1) % totalSlides;
+        document.querySelector('.slider-container').style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(showNextSlide, 3000);  // Change slide every 3 seconds
+</script>
